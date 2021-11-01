241743e821fdc1baab95b1c02afe49729fda1784
Tan Jing Long <tanjinglong0807@gmail.com>
2021-11-01 22:08:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,8 @@ st.header('Part II: Data Analysis')
 st.write('In Part II of the experiment, you will be processing the collected data and inferring the relationship between the period and length of the pendulum.')
 
 st.subheader('Procedure')
-st.markdown("5. Calculate the average time for 20 oscillations, usng the formula $<t>=(t_1+t_2)/2$. Hence, calculate the period of the pendulum $T = <t>/20$ and $T^2$.")
+st.markdown("5. Calculate the average time for 20 oscillations, usng the formula $<t>=(t_1+t_2)/2$. (Why do we have to take two readings and find their average?) Hence, calculate the period of the pendulum $T = <t>/20$ and $T^2$. (Why couldn't we have just timed one oscillation?) 
+st.write('Return yours answers to these questions and those in Part III, alongside screen captures of this page to your teacher.')")
 t = (t1+t2) / 2
 T = t / 20
 T2 = T ** 2
@@ -136,7 +137,6 @@ if st.button('Draw'):
     
 st.header('Part III: Questions')
 st.write('In Part III, you wil be using the inferred relationship between the dependent and independent variable to make some predictions.')
-st.write('Return yours answers to these questions, alongside screen captures of this page to your teacher.')
 
 st.markdown('1. From the graph, estimate the length of the pendulum that will yield a period of 2.0 s')
 st.markdown('2. The formula for the period of a pendulum is $2\pi\sqrt{l/g}$, where $l$ is the length of the pendulum. Derive the value of $g$, including its units and comment on its physical significiance.')
