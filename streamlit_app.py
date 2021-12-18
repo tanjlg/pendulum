dc24c343e4a2a045f504a53dfc3609cd3021ecb2
Tan Jing Long <tanjinglong0807@gmail.com>
2021-12-18 15:53:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,6 +75,11 @@ df = pd.DataFrame({'length/cm': length, 't1/s': t1, 't2/s': t2, '<t>/s': tave, '
 
 st.write(df)
 
+@st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
 csv_raw = convert_df(df)
 st.download_button(
      label="Download data as CSV",
