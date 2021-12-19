b613dad6182c22aa7cacf510e0591eb167a95062
Tan Jing Long <tanjinglong0807@gmail.com>
2021-12-19 11:07:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,8 +125,8 @@ if st.button('Plot'):
     st.pyplot(fig)
     
 st.markdown('8. By manipulating the line to minimise the error value, deduce the relationship between the period $T$ and length $l$ of the pendulum.')
-m = st.slider('Gradient', min_value=0.00, max_value=0.10, value=0.04, step=0.001)
-c = st.slider('Intercept (vertical)', min_value=-0.20, max_value=0.20, value=0.0, step=0.02)
+m = st.slider('Gradient', min_value=0.00, max_value=0.10, value=0.04, step=0.0001)
+c = st.slider('Intercept (vertical)', min_value=-0.20, max_value=0.20, value=0.0, step=0.01)
 # sum-of-squared error
 residuals = T2-(m*length+c)
 cost = np.sum(residuals**2)
