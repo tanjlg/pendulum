84679880c7b76ec5db536e1e2ae40da6e9bc13f1
Tan Jing Long <tanjinglong0807@gmail.com>
2021-12-31 16:33:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ plt.ylabel('Period^2/ s^2')
 if st.button('Plot'):
     st.pyplot(fig)
     
-st.markdown('10. By manipulating the line to minimise the error value, deduce the relationship between the period $T$ and length $l$ of the pendulum.')
+st.markdown('10. By manipulating the line to minimise the error value, deduce the relationship between the period $T$ and length $l$ of the pendulum. The root-mean-square deviation is similar to the standard deviation except it calculates the sum of squared distances from the hypothesised interpolated $y$ value instead of the mean.')
 m = st.slider('Gradient', min_value=0.00, max_value=0.10, value=0.04, step=0.0001)
 c = st.slider('Intercept (vertical)', min_value=-0.20, max_value=0.20, value=0.0, step=0.01)
 # sum-of-squared error
